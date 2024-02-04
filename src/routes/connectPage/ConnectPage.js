836bc463fc63924a3c13da637eaf23925a359010
Alexey Minimulin <a.s.minimulin@gmail.com>
2024-02-04 07:09:51 +0300

    Update welcome page to new design

--- src/routes/connectPage/ConnectPage.js
+++ src/routes/connectPage/ConnectPage.js
@@ -1,4 +1,3 @@
-import { FaArrowRight, FaPlus } from "components/icons";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import actions from "redux/actions";
@@ -15,105 +14,49 @@ function ConnectPage() {
   }, []);
 
   return (
-    <Box className={classes.loginRoot}>
+   <Box className={classes.loginRoot}>
       <Box className="connect-bg ">
         <Container maxWidth="xl" sx={{ height: "100%" }}>
           <Box
             sx={{ width: "100%", height: "100%" }}
-            className="   d-flex-column corner-circle-transparent"
-          >
+            className=" d-flex-column corner-circle-transparent ">
             <Box
               sx={{ width: "100%", height: "100%" }}
               className=" inner-container d-flex "
             >
-              <Box className=" loginContent  d-flex-column ">
-                <Box
-                  sx={{ flex: "auto", justifyContent: "center" }}
-                  className="  d-flex-column"
-                >
-                  <Box className=" d-flex-column" sx={{ direction: "ltr" }}>
-                    <div className=" d-flex">
-                      <Typography
-                        component="h1"
-                        variant="h1"
-                        className="white display-text"
-                      >
-                        {Object.translate("FULL_SENTENCE.LILAC_MEET")}
-                      </Typography>
-                    </div>
-
-                    <Typography className="white " component="h5" variant="h5">
-                      {Object.translate("FULL_SENTENCE.U_R_CONNECTED")}
-                    </Typography>
-                  </Box>
-
-                  <Box
-                    sx={{ alignItems: "baseline" }}
-                    className=" signup d-flex"
+              <Box className=" app-name d-flex-column center-content ">
+                <Box className=" d-flex ">
+                  <Typography
+                    variant="h1"
+                    component="h1"
                   >
-                    <Typography className="white" marginY={5}>
-                      {Object.translate("MAIN.NO_ACCOUNT")}
-                    </Typography>
-                    &nbsp; &nbsp;
-                    <Link className="blue" variant="text" to="/signup">
-                      {Object.translate("BUTTONS.SIGNUP")}
-                    </Link>
-                  </Box>
+                    Conf
+                  </Typography>
+                  <img src="/meet/logo.png"/>
+                  <Typography
+                    component="h1"
+                    variant="h1"
+                  >
+                   Meet
+                  </Typography>
                 </Box>
               </Box>
 
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  flex: "auto",
-                }}
-                className="buttons-container d-flex"
-              >
-                <div className=" d-flex">
-                  <Link
-                    variant="text"
-                    className="loginBtn white center-content d-flex-column"
-                    to="/join"
-                  >
-                    <span className="icon d-flex center-content">
-                      <FaPlus />
-                    </span>
-                    <div className="button-text">
-                      {Object.translate("BUTTONS.JOIN")}
+              <Box className=" buttons-container d-flex ">
+                <Box className=" d-flex-column ">
+                  <Link to="/login">
+                    <div className=" button-shape d-flex ">
+                      <span className=" button-text ">{Object.translate("BUTTONS.SIGNIN")}</span>
                     </div>
                   </Link>
-                  <Link
-                    variant="text"
-                    className="loginBtn white center-content d-flex-column"
-                    to="/login"
-                  >
-                    <span className="icon d-flex center-content">
-                      <FaArrowRight />
-                    </span>
-                    <div className="button-text">
-                      {Object.translate("BUTTONS.SIGNIN")}
+                  <Link to="/join">
+                    <div className=" button-shape ">
+                      <span className=" button-text ">{Object.translate("BUTTONS.JOIN")}</span>
                     </div>
-                  </Link>
-                </div>
-                <Box
-                  sx={{ alignItems: "baseline" }}
-                  className=" signup-mobile d-flex"
-                >
-                  <Typography className="white" marginY={3} marginRight={2}>
-                    {Object.translate("MAIN.NO_ACCOUNT")}
-                  </Typography>
-                  <Link className="blue" variant="text" to="/signup">
-                    {Object.translate("BUTTONS.SIGNUP")}
                   </Link>
                 </Box>
               </Box>
             </Box>
-            <div className="d-flex-column custom-footer">
-              <Box className="lilacLogo">
-                <img src={`${window.officialWhiteLogo}`} alt="lilacLogo" />
-              </Box>
-            </div>
           </Box>
         </Container>
       </Box>
--- src/routes/connectPage/style.js
+++ src/routes/connectPage/style.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@mui/styles";
-import loginBg from "../../assets/images/connectBg.png";
-import connect from "../../assets/images/connect.png";
+import loginBg from "../../assets/images/connectBg.jpg";
+import logoBg from "../../assets/images/logo.png";
 
 const connectStyles = makeStyles((theme) => ({
   loginRoot: {
@@ -10,227 +10,70 @@ const connectStyles = makeStyles((theme) => ({
     overflow: "hidden",
     display: "flex",
     marginTop: "-200px",
+    fontFamily: "Marmelad",
+    color: theme.palette.background.default,
     "& h1": {
-      fontSize: theme.globals.fontSize.xl * 2.5,
+      fontSize: "70px",
+      fontWeight: "400",
     },
-    "& .signup-mobile": {
-      display: "none",
+    "& img": {
+        aspectRatio: "1/1",
+        maxHeight: "70px",
     },
-    "& .custom-footer": {
-      alignItems: "baseline",
-      color: "#fff8",
-      fontSize: theme.globals.fontSize.xs + 1,
-      height: "85px",
-      justifyContent: "center",
-    },
-    "& .lilacLogo": {
-      display: "flex",
-      justifyContent: theme.direction === "rtl" ? "flex-end" : "flex-start",
-      [theme.breakpoints.down("600")]: {
-        width: 100,
+    "& .buttons-container": {
+      justifyContent: "end",
+      marginTop: "10vh",
+      "& >div": {
+        alignItems: "end",
       },
-      [theme.breakpoints.up("600")]: {
-        marginBottom: "70px",
-      },
+      "& a": {
+        marginRight: "calc(-185px / 2)",
+        maxWidth: "545px",
+        "&:last-child": {
+          marginRight: "calc(185px / 2 + 15px)",  // margin for alignment + margin for some space between items
+          marginTop: "calc(15px)",  // margin for some space between items
+        }
+      }
     },
-    "& .white": {
-      color: theme.palette.background.default,
-      textAlign: "left",
-    },
-    "& .blue": {
-      color: theme.globals.colors.bgBlue,
-    },
-
-    "& .loginBtn": {
-      background:
-        "linear-gradient(0deg, #288FCE 30.73%, rgba(56, 197, 235, 0) 100%)",
-      transform: "translateX(22px) perspective(600px) rotateY(-25deg)",
-      textTransform: "capitalize",
-      color: theme.globals.colors.textwhite,
-      fontSize: theme.globals.fontSize.xl,
-      fontWeight: 700,
-      height: "40vh",
-      maxHeight: 340,
-      width: "15vw",
-      maxWidth: 300,
-      minWidth: 237,
-      margin: theme.spacing("auto", 2, "auto", 0),
-      padding: theme.spacing(2),
-      textAlign: "center",
-      backdropFilter: "blur(1px)",
-      borderRadius: 30,
-      position: "relative",
-      zIndex: 1,
-      bottom: 0,
+    "& .button-shape": {
       transition: "all 0.5s ease",
-      "& .button-text": {
-        width: 177,
-        wordBreak: "break-word",
-      },
-      "& .icon": {
-        backgroundColor: "#fff",
-        width: "40px",
-        height: "40px",
-        borderRadius: "13px",
-        color: "#288fce",
-        fontSize: theme.globals.fontSize.xs,
-        marginBottom: theme.spacing(3),
-      },
-      "&:before": {
-        content: '""',
-        padding: "2px",
-        position: "absolute",
-        background:
-          "linear-gradient(205deg , #ffffff, #ffffff4d 46%,#ffffff00 76%)",
-        WebkitMask:
-          "linear-gradient(#fff 0 0) content-box,linear-gradient(#fff 0 0)",
-        borderRadius: "30px",
-        WebkitMaskComposite: "clear",
-        width: "100%",
-        height: "98%",
-        top: -20,
-        right: theme.direction === "rtl" ? "auto" : -20,
-        left: theme.direction === "rtl" ? -20 : "auto",
-        zIndex: -1,
-        transition: "all 0.5s ease",
-      },
-
-      "&:hover": {
-        transform: "translateX(22px) perspective(600px) rotateY(0deg)",
-        "&:before": {
-          top: 0,
-          right: 0,
-          left: 0,
-          height: "100%",
-        },
-      },
-    },
-    "& .loginBtn:first-child": { right: "8%", top: 50 },
-    "& .loginBtn:last-child": { right: "4%", top: -10 },
-    "& .stars-layer": {
-      position: "absolute",
-      width: "30%",
-      height: "30%",
-      zIndex: "4",
-      padding: theme.spacing(3),
-
-      "& .star": {
-        borderRadius: "50%",
-        backgroundColor: "#fff",
-        position: "absolute",
-        animationName: "twinkle",
-        animationDuration: "1s",
-        animationIterationCount: 20,
-      },
-      "& .small": {
-        width: 4,
-        height: 4,
-        animationDelay: 100,
-      },
-      "& .mid": {
-        width: 4.5,
-        height: 4.5,
-        animationDelay: 200,
-      },
-      "& .big": {
-        width: 5.5,
-        height: 5.5,
-      },
-      "& span:first-child": {
-        top: "25%",
-        right: 0,
-      },
-      "& span:nth-child(2)": {
-        top: "31%",
-        right: "25%",
-      },
-      "& span:nth-child(3)": {
-        right: "60%",
-        bottom: "50%",
-      },
-
-      "& span:nth-child(4)": {
-        right: "50%",
-      },
-      "& span:nth-child(5)": {
-        top: "56%",
-        left: "11%",
-      },
-      "& span:nth-child(6)": {
-        bottom: 38,
-        left: "8%",
-      },
-      "& span:nth-child(7)": { bottom: "0", left: "15%" },
-      "& span:nth-child(8)": {
-        left: "19%",
-        bottom: "63%",
-      },
-    },
-    "& .input": {
-      margin: "8px 0",
-
-      "& .MuiFormLabel-colorPrimary": {
-        color: "#cccc",
-      },
-      "& :-webkit-autofill": {
-        transition: "background-color 5000s ease-in-out 0s",
-      },
-      "& .MuiInputBase-root .MuiInputBase-input:-webkit-autofill": {
-        WebkitBoxShadow: "none",
-      },
-      "& .MuiInputAdornment-root": {
-        "& .MuiButtonBase-root": {
-          padding: 0,
-        },
-      },
-      "& .MuiOutlinedInput-notchedOutline": {
-        borderBottom: `1px solid ${theme.globals.colors.textLight}`,
-        borderRadius: 0,
-      },
-      "& .MuiFormHelperText-root": {
-        textAlign: "start",
-      },
-    },
-    "& .submitBtn": {
-      backgroundColor: theme.globals.colors.primary,
-      color: theme.globals.colors.textWhite,
-      width: 400,
-      height: 60,
-      textTransform: "capitalize",
-      borderRadius: 7,
-      transition: "all .35s",
+      width: 545,
+      height: 185,
+      justifyContent: "center",
       position: "relative",
-      zIndex: "0",
-      "&:hover": { color: "#fff" },
-      "&:hover:after": {
+      "&:before": {
+        content: `""`,
+        display: "block",
+        height: "100%",
         width: "100%",
+        transform: "matrix(1, 0, -1, 1, 0, 0)",
+        border: "2px #fff solid",
+        borderRight: "none",
+        borderTop: "none",
       },
     },
-    "& .Mui-disabled.submitBtn": {
-      backgroundColor: `${theme.globals.colors.disabled}!important`,
-      color: theme.globals.colors.secondary,
+    "& .button-text": {
+      wordBreak: "break-word",
+      display: "block",
+      position: "absolute",
+      bottom: "10%",
+      left: "5%",
+      textAlign: "center",
+      color: "#fff",
+      fontSize: "48px",
+      flexShrink: 0,
     },
     "& .corner-circle-transparent": {
-      "&:before": {
-        position: "absolute",
-        content: '""',
-        width: 660,
-        height: 660,
-        borderRadius: "50%",
-        border: `110px solid ${theme.palette.background.default}2`,
-        bottom: -300,
-        right: -300,
-        zIndex: 1,
-      },
       "&:after": {
         position: "absolute",
         content: '""',
         width: 660,
         height: 660,
-        borderRadius: "50%",
-        border: `110px solid ${theme.palette.background.default}2`,
-        top: -300,
-        left: -300,
+        background: `url(${logoBg})`,
+        opacity: .1,
+        bottom: -100,
+        right: -90,
+        zIndex: 1,
       },
     },
     "& .connect-bg": {
@@ -240,202 +83,57 @@ const connectStyles = makeStyles((theme) => ({
       zIndex: 3,
       height: "100%",
       width: "100%",
-      "&:before": {
-        position: "absolute",
-        content: '""',
-        inset: 0,
-        backgroundImage:
-          "linear-gradient(89.41deg, #012241 0.51%, rgba(1, 34, 65, 0.83) 62.64%, #012241 99.53%)",
-        zIndex: -1,
-      },
       "& .MuiContainer-root": {
         width: "94%",
       },
-      "& .gradient, .bordered , .image": {
-        position: "absolute",
-        transform: "translateX(22px) perspective(600px) rotateY(10deg)",
-        width: 370,
-        minWidth: 300,
-        maxHeight: "65vh",
-        margin: "auto",
-        overflow: "hidden",
-        borderRadius: 30,
-        display: "flex",
-        bottom: 0,
-        [theme.breakpoints.between("900", "1200")]: {
-          width: "83%",
-          maxHeight: "50%",
-        },
-      },
-      "& .gradient ": {
-        top: -100,
-        right: -45,
-        background:
-          "linear-gradient(180deg, #82acc5 6.62%, rgba(4, 162, 205, 0) 100%)",
-        [theme.breakpoints.between("900", "1200")]: {
-          right: 55,
-        },
-      },
-      "& .image ": {
-        top: 142,
-        right: -155,
-        background: `url(${connect}) no-repeat center center`,
-        backgroundSize: 400,
-        minHeight: 500,
-        [theme.breakpoints.between("900", "1200")]: {
-          right: -24,
-          top: 110,
-        },
-      },
-      "& .bordered": {
-        top: 7,
-        right: -102,
-        margin: "auto",
-        position: "absolute",
-        border: `4px solid ${theme.palette.background.default}`,
-        [theme.breakpoints.between("900", "1200")]: {
-          right: 14,
-        },
-      },
     },
 
-    "& .loginContent": {
-      height: "100%",
-
-      "& .link": {
-        fontSize: theme.globals.fontSize.s,
-      },
+    "& .inner-container": {
+      flexFlow: "column nowrap",
+      marginTop: "200px",
+      height: "100%"
     },
-
-    "& .PhoneInput": {
-      marginTop: "30px",
-      "& .PhoneInputInput": {
-        background: "transparent",
-        border: "none",
-        borderBottom: "1px solid #fff",
-        color: "#fff",
-        outline: "none",
-        fontSize: theme.globals.fontSize.s,
-      },
-      "& .PhoneInputCountry": {
-        color: "#fff",
-      },
-    },
-    "& .MuiFormControlLabel-root": {
-      color: theme.globals.colors.textLight,
-      "& .MuiCheckbox-root": {
-        color: theme.globals.colors.textLight,
-      },
-    },
-
-    [theme.breakpoints.down(1100)]: {
-      "& h5": {
-        textAlign: "center",
-      },
-      "& .inner-container": {
-        flexFlow: "column nowrap",
-        marginTop: 130,
-      },
-      "&  .custom-footer": {
-        height: 197,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      "& .loginContent ": {
-        height: "auto",
-
-        "& > div": {
-          justifyContent: "space-between",
-          alignItems: "center",
-        },
-      },
-      "&  .signup-mobile": {
-        display: "flex",
-        flexFlow: "column nowrap",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 66,
-        zIndex: 2,
-        "& p": {
-          margin: 0,
-        },
-      },
+    [theme.breakpoints.down("md")]: {
       "& .buttons-container": {
-        margin: "auto",
-        width: "fit-content",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
+        "& .app-name img": { maxHeight: "3.5rem" },
+        justifyContent: "end",
+        "& .button-text": { fontSize: "3.5rem" },
         "& >div": {
-          alignItems: "center",
-          width: "100%",
-        },
-        "& .loginBtn": {
-          margin: 16,
-          maxHeight: 265,
-          minWidth: 215,
-          right: "6%",
-
-          "&:before": {
-            top: "-16px!important",
-            right: "-12px!important",
+          "& a": {
+            marginRight: 0,
+            "&:last-child": {
+              marginRight: 0,
+            }
           },
+          "& .button-shape": {
+            width: "450px",
+          }
         },
-      },
-      "&  .signup": {
-        display: "none",
-      },
+      }
     },
     [theme.breakpoints.down("sm")]: {
-      "& .connect-bg": {
-        "& .MuiContainer-root": {
-          padding: 0,
-        },
+      "& .app-name": {
+        "& img": { maxHeight: "3rem" },
+        "& h1": { fontSize: "3rem" },
       },
-      "& .buttons-container": {
-        width: "100%",
-
-        "& .loginBtn": {
-          maxHeight: "230px!important",
-          minWidth: "40%!important",
-          fontSize: theme.globals.fontSize.m,
-          fontWeight: 600,
-          "& .button-text": {
-            width: 160,
-          },
-        },
-      },
-      "& h1": {
-        fontSize: "3.5rem",
-      },
-      "& h5": {
-        fontSize: theme.globals.fontSize.s * 2 - 1,
-      },
-    },
-    [theme.breakpoints.down(400)]: {
-      "& h5": {
-        fontSize: theme.globals.fontSize.xs * 2 - 1,
-      },
-      "& h1": {
-        fontSize: "3rem",
-      },
-      " & .buttons-container": {
+      " & .inner-container .buttons-container": {
+        marginTop: "30px",
+        justifyContent: "center",
         "& >div": {
-          justifyContent: "space-around",
-        },
-        "& .loginBtn": {
-          right: 0,
-          margin: 0,
-          fontSize: `${theme.globals.fontSize.s}px!important`,
-          minWidth: "140px!important",
-          maxHeight: "200px!important",
-          fontWeight: "500",
-          transform: "none!important",
-          "&:before": {
-            display: "none",
-          },
-          "& .button-text": {
-            width: "97px!important",
+          alignItems: "center",
+          "& .button-shape": {
+            width: "100%",
+            minWidth: "140px!important",
+            maxHeight: "50px!important",
+            transform: "none!important",
+            display: "block",
+            "&:before": {
+              display: "none",
+            },
+            "& .button-text": {
+              fontSize: "32px",
+              position: "static",
+            },
           },
         },
       },
